df88f49b05ac4d63de360d78d79b30fd461fe047
Muhammad Faheem <faheem0586@gmail.com>
2022-03-18 11:31:51 +0500

    Configuration Changes ..Pages Added.

---
+++ web/themes/zuvi/templates/template-parts/test.php
@@ -0,0 +1,37 @@
+<p>&nbsp;</p>
+
+<div class="full">
+  <div class="item"><img alt="image" src="/sites/default/files/inline-images/Silyzec_Large.jpg" /></div>
+
+  <div class="item">
+    <div><span style="font-size:30px; color:black!important; font-weight:400;">Silyzec </span><span style="font-size:16px;  font-weight:300;"> Tablets </span></div>
+
+    <hr />
+    <h4 style="color:black!important;">Indications</h4>
+
+    <ul style="background-color:wight!important; color:grey;">
+      <li>Chronic Hepatitus B &amp; C</li>
+      <li>High ALT, AST, Cirrhosis &amp; HCC</li>
+      <li>Aids as managing diabetes</li>
+      <li>Efficacy in controlling blood glucose level.</li>
+      <li>Efficacy in hepatic disease, digestive disorders &amp; alcoholic liver disease</li>
+      <li>Drug induced hepatic toxicity</li>
+      <li>Useful in delaying onset of dementia and alzheimers disease</li>
+    </ul>
+
+    <hr />
+    <h4 style="color:black!important;">Composition</h4>
+
+    <ul style="background-color:wight!important;color:grey;">
+      <li>Silaymarin Ext (BP) 200mg</li>
+      <li>Zinc 10mg</li>
+      <li>Vitamin E 10mg</li>
+      <li>Vitamin C 30mg</li>
+      <li>Selenium 33mcg</li>
+    </ul>
+
+    <hr /><a class="button-link" href="#" style="background-color:grey!important;">Download Details</a>
+  </div>
+</div>
+
+<p>&nbsp;</p>
